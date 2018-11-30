36cc31ea7a26d48f92324379dbef1d0a4425a5cc
Ramadani <email.ramadani@gmail.com>
2018-11-30 16:52:06 +0700

    get values from query

--- querier/querier.go
+++ querier/querier.go
@@ -1,5 +1,10 @@
 package querier
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Querier struct{}
 
 func (q *Querier) Query(stmt string) {
@@ -14,16 +19,51 @@ func (q *Querier) isQueryType1(arr []string) bool {
 	for i, s := range arr {
 		if s == "is" {
 			isIdx = i
+			break
 		}
 	}
 
 	return isIdx == (len(arr)-1)-1
 }
 
+// Get values from query type 1
+func (q *Querier) getQueryType1Values(arr []string) (string, string) {
+	var aliases []string
+	num := arr[len(arr)-1]
+
+	for _, s := range arr {
+		if s == "is" {
+			break
+		}
+		aliases = append(aliases, s)
+	}
+
+	return strings.Join(aliases, " "), num
+}
+
 // Query type 2 for query to set credit value by alias nums
 // Example: glob glob Silver is 34 Credits, glob prok Gold is 57800 Credits
 func (q *Querier) isQueryType2(arr []string) bool {
 	return arr[len(arr)-1] == "Credits"
+}
+
+// Get values from query type 2
+func (q *Querier) getQueryType2Values(arr []string) (string, string, int) {
+	var aliases []string
+
+	for _, s := range arr {
+		if s == "is" {
+			break
+		}
+		aliases = append(aliases, s)
+	}
+
+	alias := aliases[:len(aliases)-1]
+	creditType := aliases[len(aliases)-1]
+	sTotal := arr[len(arr)-2]
+	total, _ := strconv.Atoi(sTotal)
+
+	return strings.Join(alias, " "), creditType, total
 }
 
 // Query type 3 for query to ask how munch is alias nums
@@ -46,6 +86,22 @@ func (q *Querier) isQueryType3(arr []string) bool {
 	return howIdx == 0 && muchIdx == 1 && isIdx == 2
 }
 
+// Get values from query type 3
+func (q *Querier) getQueryType3Values(arr []string) string {
+	var aliases []string
+
+	arr = arr[:len(arr)-1]
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		if arr[i] == "is" {
+			break
+		}
+		aliases = append([]string{arr[i]}, aliases...)
+	}
+
+	return strings.Join(aliases, " ")
+}
+
 // Query type 4 for query to ask how many credits by alias nums and credit type
 // Example: how many Credits is glob prok Silver ?
 func (q *Querier) isQueryType4(arr []string) bool {
@@ -64,6 +120,25 @@ func (q *Querier) isQueryType4(arr []string) bool {
 	}
 
 	return howIdx == 0 && manyIdx == 1 && creditsIdx == 2
+}
+
+// Get values from query type 4
+func (q *Querier) getQueryType4Values(arr []string) (string, string) {
+	var aliases []string
+
+	arr = arr[:len(arr)-1]
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		if arr[i] == "is" {
+			break
+		}
+		aliases = append([]string{arr[i]}, aliases...)
+	}
+
+	alias := aliases[:len(aliases)-1]
+	creditType := aliases[len(aliases)-1]
+
+	return strings.Join(alias, " "), creditType
 }
 
 func NewQuerier() *Querier {
--- querier/querier_test.go
+++ querier/querier_test.go
@@ -112,3 +112,102 @@ func TestQueryType4(t *testing.T) {
 		}
 	}
 }
+
+func TestGetQueryType1Values(t *testing.T) {
+	type queryType1Value struct {
+		stmt  string
+		alias string
+		num   string
+	}
+
+	tests := []queryType1Value{
+		queryType1Value{"glob is I", "glob", "I"},
+		queryType1Value{"prok is V", "prok", "V"},
+		queryType1Value{"pish is X", "pish", "X"},
+		queryType1Value{"tegj is L", "tegj", "L"},
+	}
+
+	querier := NewQuerier()
+
+	for _, tt := range tests {
+		alias, num := querier.getQueryType1Values(strings.Split(tt.stmt, " "))
+
+		if alias != tt.alias || num != tt.num {
+			t.Errorf("TestGetQueryType1Values failed, expected: '%s %s', got: '%s %s'",
+				tt.alias, tt.num, alias, num)
+		}
+	}
+}
+
+func TestGetQueryType2Values(t *testing.T) {
+	type queryType2Value struct {
+		stmt       string
+		aliases    string
+		creditType string
+		total      int
+	}
+
+	tests := []queryType2Value{
+		queryType2Value{"glob glob Silver is 34 Credits", "glob glob", "Silver", 34},
+		queryType2Value{"glob prok Gold is 57800 Credits", "glob prok", "Gold", 57800},
+		queryType2Value{"pish pish Iron is 3910 Credits", "pish pish", "Iron", 3910},
+	}
+
+	querier := NewQuerier()
+
+	for _, tt := range tests {
+		aliases, creditType, total := querier.getQueryType2Values(strings.Split(tt.stmt, " "))
+
+		if aliases != tt.aliases || creditType != tt.creditType || total != tt.total {
+			t.Errorf("TestGetQueryType2Values failed, expected: '%s %s %d', got: '%s %s %d'",
+				tt.aliases, tt.creditType, tt.total, aliases, creditType, total)
+		}
+	}
+}
+
+func TestGetQueryType3Values(t *testing.T) {
+	type queryType3Value struct {
+		stmt  string
+		alias string
+	}
+
+	tests := []queryType3Value{
+		queryType3Value{"how much is tegj pish ?", "tegj pish"},
+		queryType3Value{"how much is pish tegj glob glob ?", "pish tegj glob glob"},
+	}
+
+	querier := NewQuerier()
+
+	for _, tt := range tests {
+		alias := querier.getQueryType3Values(strings.Split(tt.stmt, " "))
+
+		if alias != tt.alias {
+			t.Errorf("TestGetQueryType3Values failed, expected: '%s', got: '%s'", tt.alias, alias)
+		}
+	}
+}
+
+func TestGetQueryType4Values(t *testing.T) {
+	type queryType4Value struct {
+		stmt       string
+		alias      string
+		creditType string
+	}
+
+	tests := []queryType4Value{
+		queryType4Value{"how many Credits is glob prok Silver ?", "glob prok", "Silver"},
+		queryType4Value{"how many Credits is pish prok Gold ?", "pish prok", "Gold"},
+		queryType4Value{"how many Credits is glob pish Iron ?", "glob pish", "Iron"},
+	}
+
+	querier := NewQuerier()
+
+	for _, tt := range tests {
+		alias, creditType := querier.getQueryType4Values(strings.Split(tt.stmt, " "))
+
+		if alias != tt.alias || creditType != tt.creditType {
+			t.Errorf("TestGetQueryType3Values failed, expected: '%s %s', got: '%s %s'",
+				tt.alias, tt.creditType, alias, creditType)
+		}
+	}
+}
